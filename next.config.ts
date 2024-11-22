95585016b7f516359f283729ca8c00a84073a97c
AugustoV2 <augustovarghese@gmail.com>
2024-11-22 22:09:27 +0530

    face-api

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config: any, { isServer }: { isServer: boolean }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+    return config;
+  },
 };
-
-export default nextConfig;
